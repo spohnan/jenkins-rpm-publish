812fa2489dd6d38ab868037b12af2af3cb359c32
Andy Spohn <Andy@AndySpohn.com>
2013-01-04 08:51:02 -0500

    First attempt at the actual publish script with some tests

--- SOURCES/jenkins-rpm-publish/usr/local/bin/jenkins-publish-rpm.sh
+++ SOURCES/jenkins-rpm-publish/usr/local/bin/jenkins-publish-rpm.sh
@@ -8,15 +8,90 @@
 #  - Optionally, it will also optionally truncate the number of versions a single rpm kept in the repo
 
 USAGE_ERROR_STR="Usage: jenkins-publish-rpm.sh REPO_DIR RPM_NAME [MAX_VERSIONS]"
+RPM_REPO_DIR_ERROR_STR="Could not locate or access rpm repo directory argument"
+MAX_RPM_VERSION_ERROR_STR="The max number of rpm versions to keep argument must be a positive integer"
 ERROR_STR=
+
+# A special value used for the RPM name that will allow for testing and not actually publish
+TEST_RUN="THIS_IS_ONLY_A_TEST"
 
 RPM_REPO_DIR="$1"
 RPM_NAME="$2"
 MAX_RPM_VERSIONS=$3
+NUM_ARGUMENTS=$#
 
+# This is the main purpose of this script
+publishRpm() {
 
-# Have to have at least the mandatory arguments present
-if [ $# -lt 2 ] ; then
+    # Copy the file to the right place
+    cp -f $RPM_NAME $RPM_REPO_DIR
+
+    # If the optional max versions argument was passed
+    if ( isPositiveIntegerValue "$MAX_RPM_VERSIONS" ) ; then
+        truncateNumVersions $RPM_REPO_DIR $RPM_NAME $MAX_RPM_VERSIONS
+    fi
+
+    # Update the RPM repo (use sqlite db and only update changes if possible)
+    createrepo --database --update $RPM_REPO_DIR
+}
+
+# truncateNumVersions PATH, RPM_NAME, NUM_VERSIONS_TO_KEEP
+#   Used to get rid of older versions of the same RPM
+truncateNumVersions() {
+    # Sanity check the arguments a bit
+    if [ -d $1 ] && isPositiveIntegerValue $3 ; then
+        # List in time sequence with newest on top, use tail to figure out how many
+        # of the oldest files we want to chop from the bottom of the list and pipe
+        # to the remove command
+        ls -t $1/$2-*.rpm | tail -n +$(($3+1)) | xargs rm -f
+    fi
+}
+
+isPositiveIntegerValue() {
+    # Is it a numeric integer
+    if [[ "$1" =~ ^[0-9]+$ ]] ; then
+        # Is it greater than or equal to 1
+        [[ "$1" -ge 1 ]] && return 0 || return 1
+    else
+        # Nope
+        return 1
+    fi
+}
+
+# Some reading online about edge cases with empty string comparisons led me to encapsulate
+# the "add an extra char" recommended solution in this method
+isStringValPresent() {
+    [ "x$1" != "x" ] && return 0 || return 1
+}
+
+# If the name of the RPM is a specific value then this is being run by the test suite
+isTestRun() {
+    [ "$RPM_NAME" = "$TEST_RUN" ] && return 0 || return 1
+}
+
+# Check the number of arguments
+if [[ $NUM_ARGUMENTS -ge 2 && $NUM_ARGUMENTS -le 3 ]] ; then
+
+    # Check that the repo dir given actually exists
+    if [ ! -d $RPM_REPO_DIR ] ; then
+        ERROR_STR=$RPM_REPO_DIR_ERROR_STR
+    fi
+
+    # Check using the numeric comparison to check against itself, will error if not a number
+    if ( isStringValPresent "$MAX_RPM_VERSIONS" && ! isPositiveIntegerValue "$MAX_RPM_VERSIONS" ) ; then
+        ERROR_STR=$MAX_RPM_VERSION_ERROR_STR
+    fi
+
+    if ( ! isStringValPresent "$ERROR_STR" &&  ! isTestRun ) ; then
+        # Good to go ... run the main method of the script
+        publishRpm
+
+    else
+        # Something wrong, return error string
+        echo $ERROR_STR
+    fi
+
+else
     ERROR_STR=$USAGE_ERROR_STR
     echo $USAGE_ERROR_STR
 fi
--- TESTS/run-tests.sh
+++ TESTS/run-tests.sh
@@ -3,13 +3,160 @@
 # These are all present as globals in , here just to silence
 # local IDE "errors"
 USAGE_ERROR_STR=
+RPM_REPO_DIR_ERROR_STR=
+MAX_RPM_VERSION_ERROR_STR=
+TEST_RUN=
 ERROR_STR=
+NO_ERRORS=""
+
+
+# max number of rpm version truncation tests
+# ----------------------------------------------------------------------
+testMaxVersionTruncation() {
+    local MY_RPM_NAME="cool-stuff"
+    local MAX_NUM_VERSIONS=2
+
+    # Set up new files, instruct to keep only the two latest
+    makeTenRpmFiles $MY_RPM_NAME
+    truncateNumVersions $TEST_TMPDIR $MY_RPM_NAME $MAX_NUM_VERSIONS
+
+    local NUM_FILES_REMAINING=$(ls -1 $TEST_TMPDIR | wc -l |  tr -d ' ')
+    assertEquals "Should be $MAX_NUM_VERSIONS files left out of the $NUM_TEST_RPM_FILES test files" \
+        $MAX_NUM_VERSIONS $NUM_FILES_REMAINING
+}
+
+testMaxVersionTruncationWithBadNumberArg() {
+    local MY_RPM_NAME="cool-stuff"
+    local MAX_NUM_VERSIONS=foo
+
+    # Set up new files, instruct to keep only the two latest
+    makeTenRpmFiles $MY_RPM_NAME
+    truncateNumVersions $TEST_TMPDIR $MY_RPM_NAME $MAX_NUM_VERSIONS
+
+    local NUM_FILES_REMAINING=$(ls -1 $TEST_TMPDIR | wc -l |  tr -d ' ')
+    assertEquals "Should be all $NUM_TEST_RPM_FILES files left as we gave a bad numeric argument" \
+        $NUM_TEST_RPM_FILES $NUM_FILES_REMAINING
+}
+
+testMaxVersionTruncationWithSeveralRpms() {
+    local RPM_ONE_NAME="cool-stuff"
+    local RPM_TWO_NAME="cool-stuff2"
+    local MAX_NUM_VERSIONS=4
+
+    # Set up both sets of files but only truncate one of them
+    makeTenRpmFiles $RPM_ONE_NAME
+    makeTenRpmFiles $RPM_TWO_NAME
+    truncateNumVersions $TEST_TMPDIR $RPM_ONE_NAME $MAX_NUM_VERSIONS
+
+    # Count up the number of each type of file remaining
+    local NUM_RPM_ONE_FILES_REMAINING=$(ls -1 $TEST_TMPDIR/$RPM_ONE_NAME-*.rpm | wc -l |  tr -d ' ')
+    local NUM_RPM_TWO_FILES_REMAINING=$(ls -1 $TEST_TMPDIR/$RPM_TWO_NAME-*.rpm | wc -l |  tr -d ' ')
+
+    # Check to ensure we only deleted only one type of rpm
+    assertEquals "Should be $MAX_NUM_VERSIONS files left after calling truncate" \
+            $MAX_NUM_VERSIONS $NUM_RPM_ONE_FILES_REMAINING
+
+    assertEquals "Should be all $NUM_RPM_TWO_FILES_REMAINING files left as we did not call truncate" \
+        $NUM_TEST_RPM_FILES $NUM_RPM_TWO_FILES_REMAINING
+}
+
+
+# utility routine tests
+# ----------------------------------------------------------------------
+
+testBigIntegerValue() {
+    assertTrue "A single number should return true" "isPositiveIntegerValue 123409871023432"
+}
+
+testSingleIntegerValue() {
+    assertTrue "A single number should return true" "isPositiveIntegerValue 9"
+}
+
+testZeroIntegerValue() {
+    assertFalse "A zero is not a positive integer" "isPositiveIntegerValue 0"
+}
+
+testNegativeIntegerValue() {
+    assertFalse "A negative integer should return false" "isPositiveIntegerValue -1"
+}
+
+testAlphaNumericValue() {
+    assertFalse "Letters are not numeric" "isPositiveIntegerValue A"
+}
+
+testBlankValue() {
+    assertFalse "An empty value should not be considered numeric" "isPositiveIntegerValue "
+}
+
+testEmptyStringValue() {
+    assertFalse "Empty string should return false" "isStringValPresent "
+}
+
+testStringValue() {
+    assertTrue "Empty string should return false" "isStringValPresent foo"
+}
+
+# argument parsing tests
+# ----------------------------------------------------------------------
+
+testNonExistentDirectoryArg() {
+    . $SCRIPT_TO_BE_TESTED /tmp/ab.1231232.i.do.not.exist foo2 5 >/dev/null 2>&1
+    assertEquals "Rpm directory arg must refer to an actual directory" \
+        "$RPM_REPO_DIR_ERROR_STR" "$ERROR_STR"
+}
+
+testNonNumericMaxNumRpmsArg() {
+    . $SCRIPT_TO_BE_TESTED /tmp foo2 foo3 >/dev/null 2>&1
+    assertEquals "Third argument should be numeric if present" \
+        "$MAX_RPM_VERSION_ERROR_STR" "$ERROR_STR"
+}
+
+testTooFewArgs() {
+    . $SCRIPT_TO_BE_TESTED foo1 >/dev/null 2>&1
+    assertEquals "Required args missing should result in usage error str" \
+        "$USAGE_ERROR_STR" "$ERROR_STR"
+}
+
+testTooManyArgs() {
+    . $SCRIPT_TO_BE_TESTED foo1 foo2 foo3 foo4 >/dev/null 2>&1
+    assertEquals "Too many args should result in usage error str" \
+        "$USAGE_ERROR_STR" "$ERROR_STR"
+}
 
 testMissingAllArgs() {
     . $SCRIPT_TO_BE_TESTED >/dev/null 2>&1
     assertEquals "Required args missing should result in usage error str" \
         "$USAGE_ERROR_STR" "$ERROR_STR"
 }
+
+testOnlyRequiredArgs() {
+    . $SCRIPT_TO_BE_TESTED /tmp $TEST_RUN >/dev/null 2>&1
+    assertEquals "Third argument is optional" "$NO_ERRORS" "$ERROR_STR"
+}
+
+# file system dependent tests setup, tear down and utility methods
+# ----------------------------------------------------------------------
+TEST_TMPDIR=
+oneTimeSetUp() {
+    TEST_TMPDIR=$(mktemp -d /tmp/jenkins-rpm-publish-tests.XXXXXX)
+}
+
+oneTimeTearDown() {
+    rm -fr $TEST_TMPDIR
+}
+
+tearDown() {
+    rm -fr $TEST_TMPDIR/*
+}
+
+# Given a base name for our fake rpm files create 10 of them in the tmp dir
+NUM_TEST_RPM_FILES=10
+makeTenRpmFiles() {
+    for i in {0..9}; do
+        touch -t "201301010${i}00" $TEST_TMPDIR/$1-$i.rpm
+    done
+}
+
 
 # test suite setup and execution
 # ----------------------------------------------------------------------
